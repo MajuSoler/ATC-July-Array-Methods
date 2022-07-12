5893204dae365a1b3088617727a0dd5ea4a5f99d
majusoler <juliasolernunes@ymail.com>
2022-07-12 15:55:55 +0200

    This is another bit of code

--- index.js
+++ index.js
@@ -58,6 +58,25 @@ const smartStudents = [
   },
 ];
 
-console.log(smartStudents, students);
+// console.log(smartStudents, students);
 
-// The difference of FOrEach and Map
+///For loop
+const names = ["Maria", "Jhon", "Mark", "Joana", "Anna"];
+// i++ is the same as as i=i+1
+// for (let i = 0; i < 4; i = i + 2) {
+//   console.log("This is the name", names[i]);
+// }
+
+//For Each
+
+const eachTeste = names.forEach((name) => {
+  return name;
+});
+console.log(eachTeste, "this is eachMethod");
+// The difference of ForEach and Map
+
+const array = names.map((item) => {
+  return item;
+});
+
+console.log(array, "this is map teste");
